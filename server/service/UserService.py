b1ac5cf25247dbb797a56256fa0078cea30d0d8d
matus mac <ccc@gmail.com>
2025-10-12 17:23:53 +0200

    add debug log for register user that already exists

--- server/service/UserService.py
+++ server/service/UserService.py
@@ -10,6 +10,7 @@ class UserService:
     def register_user(self, username:str, password:str) -> User:
         existing_user = self.repo.get_by_username(username)
         if existing_user:
+            print("Username already exists")
             raise HTTPException(
                 status_code = status.HTTP_400_BAD_REQUEST,
                 detail="Username already exists"
